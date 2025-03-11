5996555a3e0417181a20c09ff346e170c377782a
zeidlitz <zeidlitzerik@gmail.com>
2025-03-11 22:02:23 +0100

    Adding reddis consumer logic

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
+import redis
 import logging
 import spacy
 
-from datetime import datetime
 from keybert import KeyBERT
-from transformers import pipeline
-from tqdm import tqdm
 from transformers import pipeline
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -13,8 +11,11 @@ kw_model = KeyBERT()
 nlp = spacy.load("en_core_web_sm")
 sentiment_pipeline = pipeline("sentiment-analysis")
 
-DATA_SOURCE = "data.json"
-OUTPUT_DATA = f"{datetime.now().strftime('%Y-%m-%d-%H:%M')}-output-data.json"
+STREAM_NAME = "data_extraction"
+OUTPUT_DATA = "output.json"
+CONSUMER_GROUP = "data_analysis"
+CONSUMER_NAME = "analysis"
+redis_consumer = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 def extract_categories_with_context(text, context):
     doc = nlp(text + " " + context)
@@ -25,12 +26,23 @@ def extract_categories_with_context(text, context):
     split_list = [word for item in categories[:5] for word in item.split()]
     return list(set(split_list))
 
-def main():
-    with open(DATA_SOURCE, "r", encoding="utf-8") as f:
-        input_data = json.load(f)
+def consume_stream():
+    while True:
+        try:
+            messages = redis_consumer.xreadgroup(CONSUMER_GROUP, CONSUMER_NAME, {STREAM_NAME: '>'}, count=1, block=5000)
+            logging.info(f"Consuming messages from {STREAM_NAME}")
+            if messages:
+                logging.info(f"Message received!")
+                for stream, message_list in messages:
+                    for message_id, message in message_list:
+                        redis_consumer.xack(STREAM_NAME, CONSUMER_GROUP, message_id)
+                        return json.loads(message['data'])
+        except Exception as e:
+            print(f"Error: {e}")
 
+def analyze_data(data):
     output_data = []
-    for entry in tqdm(input_data, desc="Running analysis ..."):
+    for entry in data:
         categories = extract_categories_with_context(entry["body"], entry["posted_in"])
         sentiment_result = sentiment_pipeline(entry["body"])[0]
         output_entry = {
@@ -48,6 +60,12 @@ def main():
         json.dump(output_data, f, indent=4)
 
     logging.info("Analysis complete")
+
+
+def main():
+    while True:
+        data = consume_stream()
+        analyze_data(data)
 
 if __name__ == "__main__":
     main()
