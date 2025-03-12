6b3f10d64285a9305c3df5c4c9e83515a1235807
zeidlitz <zeidlitzerik@gmail.com>
2025-03-12 20:38:35 +0100

    Adding publisher logic

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+import os
 import json
 import redis
 import logging
 import spacy
 
+from dotenv import load_dotenv
 from keybert import KeyBERT
 from transformers import pipeline
 
+load_dotenv()
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 kw_model = KeyBERT()
 nlp = spacy.load("en_core_web_sm")
 sentiment_pipeline = pipeline("sentiment-analysis")
 
-STREAM_NAME = "data_extraction"
-OUTPUT_DATA = "output.json"
-CONSUMER_GROUP = "data_analysis"
-CONSUMER_NAME = "analysis"
-redis_consumer = redis.Redis(host='localhost', port=6379, decode_responses=True)
+CONSUMER_STREAM = os.getenv("CONSUMER_STREAM", "data_extraction")
+PRODUCER_STREAM = os.getenv("PRODUCER_STREAM ","data_analysis")
+CONSUMER_GROUP = os.getenv("CONSUMER_GROUP ","data_analysis")
+CONSUMER_NAME = os.getenv("CONSUMER_NAME","analysis")
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 def extract_categories_with_context(text, context):
     doc = nlp(text + " " + context)
@@ -29,13 +35,13 @@ def extract_categories_with_context(text, context):
 def consume_stream():
     while True:
         try:
-            messages = redis_consumer.xreadgroup(CONSUMER_GROUP, CONSUMER_NAME, {STREAM_NAME: '>'}, count=1, block=5000)
-            logging.info(f"Consuming messages from {STREAM_NAME}")
+            messages = redis_client.xreadgroup(CONSUMER_GROUP, CONSUMER_NAME, {CONSUMER_STREAM: '>'}, count=1, block=5000)
+            logging.info(f"Consuming messages from {CONSUMER_STREAM}")
             if messages:
                 logging.info(f"Message received!")
                 for stream, message_list in messages:
                     for message_id, message in message_list:
-                        redis_consumer.xack(STREAM_NAME, CONSUMER_GROUP, message_id)
+                        redis_client.xack(CONSUMER_STREAM, CONSUMER_GROUP, message_id)
                         return json.loads(message['data'])
         except Exception as e:
             print(f"Error: {e}")
@@ -54,18 +60,18 @@ def analyze_data(data):
                 "sentiment": sentiment_result["label"]
         }
         output_data.append(output_entry)
+    return json.dumps(output_data)
 
-    logging.info(f"Writing to {OUTPUT_DATA}")
-    with open(OUTPUT_DATA, "w", encoding="utf-8") as f:
-        json.dump(output_data, f, indent=4)
-
-    logging.info("Analysis complete")
+def publish_data(data):
+    logging.info(f"Publishing {len(data)} to {REDIS_HOST}:{REDIS_PORT}:{PRODUCER_STREAM}")
+    redis_client.xadd(PRODUCER_STREAM, {"data": data})
 
 
 def main():
     while True:
         data = consume_stream()
-        analyze_data(data)
+        output_data = analyze_data(data)
+        publish_data(output_data)
 
 if __name__ == "__main__":
     main()
